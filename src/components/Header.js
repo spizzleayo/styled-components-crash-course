cafab4f79d272859534ed286f53fe79aba2cc871
spizzleayo <rajiayomikun@gmail.com>
2021-02-19 18:48:13 +0200

    added started code

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Styled-components Crash Course</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h2>Lorem Ipsum</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+          et doloremque, velit molestiae officia, laboriosam provident
+          blanditiis nostrum maxime nam, totam rem voluptates amet accusantium
+          est modi. Architecto vel quidem ab odio maiores consequuntur odit
+          voluptatibus a, recusandae quam.
+        </p>
+        <button>Click me</button>
+      </div>
+      <div>
+        <h2>Lorem Ipsum</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+          et doloremque, velit molestiae officia, laboriosam provident
+          blanditiis nostrum maxime nam, totam rem voluptates amet accusantium
+          est modi. Architecto vel quidem ab odio maiores consequuntur odit
+          voluptatibus a, recusandae quam.
+        </p>
+        <button>Click me</button>
+      </div>
+      <div>
+        <h2>Lorem Ipsum</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nobis
+          et doloremque, velit molestiae officia, laboriosam provident
+          blanditiis nostrum maxime nam, totam rem voluptates amet accusantium
+          est modi. Architecto vel quidem ab odio maiores consequuntur odit
+          voluptatibus a, recusandae quam.
+        </p>
+        <button>Click me</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
